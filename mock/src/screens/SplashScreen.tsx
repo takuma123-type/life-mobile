b244d76a4dca4d0ab4b02e4cd18365643bc2c4ee
Takuma <seishirou1270@gmail.com>
2025-11-16 20:14:15 +0900

    wip

--- mock/src/screens/SplashScreen.tsx
+++ mock/src/screens/SplashScreen.tsx
@@ -66,7 +66,7 @@ const SplashScreen: React.FC = () => {
       <div style={{ textAlign:'center' }}>
         <div className="logo-container">
           <img 
-            src="../../icon/generated_image_transparent (1).png"
+            src="/icon/generated_image_transparent (1).png"
             alt="Talk! Logo" 
             style={{ 
               width: 250, 
